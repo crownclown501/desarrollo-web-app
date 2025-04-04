fc83fa810dc49e538d4ce03552700bf66d1d7640
crownclown501 <ferl52205@gmail.com>
2025-04-04 01:35:22 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
+
+//Angular material
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+//mask
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { LeftSidebarComponent } from './partials/left-sidebar/left-sidebar.component';
+import { HeaderAppComponent } from './partials/header-app/header-app.component';
+import { FooterAppComponent } from './partials/footer-app/footer-app.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegistroUsuariosScreenComponent,
+    LeftSidebarComponent,
+    HeaderAppComponent,
+    FooterAppComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    NgxMaskDirective,
+    MatSidenavModule
   ],
-  providers: [],
+  providers: [
+    provideNgxMask()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
